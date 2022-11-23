fc7aa8f4ed2469b588925c01e64d328fdf96e379
MatHartley <115046381+MatHartley@users.noreply.github.com>
2022-11-23 11:04:01 +1100

    23/11/22
    POST ACCIDENTAL SHUTDOWN SAFETY PUSH

--- Assets/Scripts/SceneRespawn.cs
+++ Assets/Scripts/SceneRespawn.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace MathewHartley
 {
-    public class DoomCube : MonoBehaviour
+    public class SceneRespawn : MonoBehaviour
     {
         private void OnCollisionEnter(Collision collision)
         {
             Debug.Log("PlayerCharacter has hit the DoomCube");
+            SceneManager.LoadScene("World");
         }
     }
 }
